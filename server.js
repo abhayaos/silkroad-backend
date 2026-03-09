586c57dc1bd19f6d7275fb96098468bc04142c41
nepalcodinghouse <nepalcodinghouse@gmail.com>
2026-03-09 17:13:44 +0545

    remove env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 8173;
+const PORT =  8173;
 
 const allowedOrigins = [
   "http://localhost:5173",   // Vite React
